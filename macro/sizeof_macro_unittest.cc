d5b58ddcc7fd2a86953fbbc0021970aecf85ad6f
zhaokai <zhaokai@yidian-inc.com>
2016-04-07 10:52:44 +0800

    Add countof() to count array size which can not accept pointer

--- macro/sizeof_macro_unittest.cc
+++ macro/sizeof_macro_unittest.cc
@@ -13,10 +13,7 @@
 
 #define ARRARY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-#define FIELD_SIZE(t, f) (sizeof(((t*)0)->f))
-
-
-TEST(USEFUL_MACRO, ARRARY_SIZE) {
+TEST(sizeof_macro, ARRARY_SIZE) {
 	int arr_int[100];
   EXPECT_EQ(100, ARRARY_SIZE(arr_int));
 
@@ -24,12 +21,34 @@ TEST(USEFUL_MACRO, ARRARY_SIZE) {
   EXPECT_EQ(101, ARRARY_SIZE(arr_char));
 }
 
+// Notice: the ARRAY_SIZE() can accept a pointer, which is wrong.
+// Here is a new macro to count array size which only accept array
+// Reference: https://blogs.msdn.microsoft.com/the1/2004/05/07/how-would-you-get-the-count-of-an-array-in-c-2/
+
+template <typename T, size_t N>
+char (&_ArraySizeHelper(T (&array) [N]) ) [N];
+
+#define countof(array) sizeof(_ArraySizeHelper(array))
+
+TEST(sizeof_macro, countof) {
+	int arr_int[100];
+  EXPECT_EQ(100, countof(arr_int));
+
+	char arr_char[101];
+  EXPECT_EQ(101, countof(arr_char));
+
+  //int *p = NULL;
+  //countof(p); // compile error
+}
+
 struct SA {
 	int age;
 	char name[10];
 };
 
-TEST(USEFUL_MACRO, FIELD_SIZE) {
+#define FIELD_SIZE(t, f) (sizeof(((t*)0)->f))
+
+TEST(sizeof_macro, FIELD_SIZE) {
   EXPECT_EQ(4, FIELD_SIZE(SA, age));
   EXPECT_EQ(10, FIELD_SIZE(SA, name));
 }
